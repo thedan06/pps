66094b0df77944ed051e6254a8bd2e3dbfc21d9d
Dustan.Mbaga <Dustan.Mbaga@nmbbank.co.tz>
2020-03-29 11:28:37 +0300

    Fetching details from an API, displaying a list of item and navigating to item details by clicking it on the list

--- app/src/main/java/com/dustanmbaga/pps/acaricide/details/AcaricideDetailsFragment.kt
+++ app/src/main/java/com/dustanmbaga/pps/acaricide/details/AcaricideDetailsFragment.kt
@@ -12,9 +12,13 @@ import com.dustanmbaga.pps.R
 class AcaricideDetailsFragment : Fragment() {
 
     private lateinit var acaricideDetailsViewModel: AcaricideDetailsViewModel
+    //private lateinit var tradeNameTitle: TextView
     private lateinit var commonName: TextView
     private lateinit var regNumber: TextView
-    private lateinit var registrar: TextView
+    private lateinit var registrant: TextView
+    private lateinit var regCategory: TextView
+    private lateinit var tradeName: TextView
+    private lateinit var usage: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,9 +28,13 @@ class AcaricideDetailsFragment : Fragment() {
 
         val root = inflater.inflate(R.layout.fragment_acaricide_details, container, false)
 
+        // tradeNameTitle = root.findViewById(R.id.tradeNameTitle)
         commonName = root.findViewById(R.id.commonName)
-        regNumber = root.findViewById(R.id.registeredName)
-        registrar = root.findViewById(R.id.registrar)
+        regNumber = root.findViewById(R.id.regNumber)
+        registrant = root.findViewById(R.id.registrant)
+        regCategory = root.findViewById(R.id.registrationCategory)
+        tradeName = root.findViewById(R.id.tradeName)
+        usage = root.findViewById(R.id.usage)
 
         val bundle = this.arguments
         if (bundle != null) {
@@ -36,7 +44,11 @@ class AcaricideDetailsFragment : Fragment() {
 
             commonName.text = commonNameValue.toString()
             regNumber.text = regNumberValue.toString()
-            registrar.text = registrarValue.toString()
+            registrant.text = registrarValue.toString()
+            //tradeNameTitle.text = bundle.get("tradeName").toString()
+            tradeName.text = bundle.get("tradeName").toString()
+            usage.text = bundle.get("usage").toString()
+            regCategory.text = bundle.get("regCategory").toString()
         }
 
         return root
